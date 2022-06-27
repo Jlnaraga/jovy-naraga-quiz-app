dfa45925fdb78c24c0e92bdddcefe8aded180e35
Jovy Ira <ira_bonching@yahoo.com>
2022-06-26 21:41:25 -0400

    remove commented questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -4,29 +4,29 @@ var questions = [
         choices: ["Brendan Eich", "Douglas Crockford", "Mark Zuckerberg", "Mitchel Baker"],
         answer: "Brendan Eich"
     },
-    // {
-    //     title: "The condition in an if / else statement is enclosed within ____.",
-    //     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    //     answer: "parentheses"
-    // },
-    // {
-    //     title: "Which built-in method removes the last element from an array and returns that element?",
-    //     choices: ["last()", "get()", "pop()", "None of the Above"],
-    //     answer: "pop()"
-    // },
-    // {
-    //     title: "Commonly used data types DO NOT include:",
-    //     choices: ["strings", "booleans", "alerts", "numbers"],
-    //     answer: "alerts"
-    // },
-    // {   
-    //     title: "Which of the following function of Number object returns the number's value",
-    //     choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
-    //     answer: "valueOf()"
-    // },
-    // {
-    //     title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
-    //     choices: ["Javascript", "terminal / bash", "for loops", "console log"],
-    //     answer: "console log"
-    // }
+    {
+        title: "The condition in an if / else statement is enclosed within ____.",
+        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "parentheses"
+    },
+    {
+        title: "Which built-in method removes the last element from an array and returns that element?",
+        choices: ["last()", "get()", "pop()", "None of the Above"],
+        answer: "pop()"
+    },
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {   
+        title: "Which of the following function of Number object returns the number's value",
+        choices: ["toString()", "valueOf()", "toLocaleString()", "toPrecision()"],
+        answer: "valueOf()"
+    },
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: "console log"
+    }
 ];
